e17ccf4ddfc7de330be7fcf89d4b7970e8712eea
Joel Gage <jrossgage@gmail.com>
2021-05-25 11:30:47 -0500

    Phase 2

--- Program.cs
+++ Program.cs
@@ -3,5 +3,17 @@
 Console.WriteLine("Guess the secret number.");
 Console.Write("Your guess? ");
 string guess = Console.ReadLine();
+int numGuess = int.Parse(guess);
+int secretNum = 42;
 
-Console.WriteLine(guess);
+if (numGuess == secretNum)
+{
+    Console.WriteLine("You guessed correctly!");
+}
+else
+{
+    Console.WriteLine("WRONG.");
+};
+
+
+
