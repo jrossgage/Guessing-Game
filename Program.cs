7b25b5b98bf1063c1e5eff1bc93763426405c92c
Joel Gage <jrossgage@gmail.com>
2021-05-25 13:01:00 -0500

    Completed

--- Program.cs
+++ Program.cs
@@ -1,19 +1,36 @@
 ﻿using System;
 
-Console.WriteLine("Guess the secret number.");
-Console.Write("Your guess? ");
-string guess = Console.ReadLine();
-int numGuess = int.Parse(guess);
-int secretNum = 42;
 
-if (numGuess == secretNum)
+int secretNum = 42;
+int attempts = 1;
+Main();
+
+void Main()
 {
-    Console.WriteLine("You guessed correctly!");
-}
-else
-{
-    Console.WriteLine("WRONG.");
+    Console.WriteLine("Guess the secret number.");
+    Console.Write("Your guess? ");
+    string guess = Console.ReadLine();
+    int numGuess = int.Parse(guess);
+
+
+    if (numGuess == secretNum && attempts < 4)
+    {
+        Console.WriteLine("You guessed correctly!");
+    }
+    else if (attempts >= 4)
+    {
+        Console.WriteLine("Wrong. No more guesses");
+    }
+    else
+    {
+        Console.WriteLine("WRONG. Try again");
+        attempts++;
+        Main();
+    };
+
 };
+
+
 
 
 
